d7b2346cff0e94e2764c21c6e2f5c96d39e8104c
Ariel Elkin <ariel@shapedbyiris.com>
2022-10-12 11:15:06 +0200

    initial commit

---
+++ Shared/TestCheckoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCheckoutApp.swift
+//  Shared
+//
+//  Created by Ariel Elkin on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestCheckoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
